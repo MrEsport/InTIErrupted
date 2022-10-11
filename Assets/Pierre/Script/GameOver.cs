54874ef417d2a531d0dc210ef46643047bbb72c0
MrEsport <lucasespositonc@gmail.com>
2022-10-11 10:37:12 +0200

    linked ButtonManager functions

--- Assets/Pierre/Script/GameOver.cs
+++ Assets/Pierre/Script/GameOver.cs
@@ -23,115 +23,13 @@ public class GameOver : MonoBehaviour
 
     public void CheckButtons()
     {
-        if (Input.GetKey(KeyCode.Q) && Input.GetKey(KeyCode.S))
-        {
-            fauteuil = false;
-            if (!lampe && !yoga)
-            {
-                lampe = true;
-                yoga = true;
-                compteurl = 0;
-                compteurl++;
-                compteury = 0;
-                compteury++;
-            }
-            else if (!lampe)
-            {
-                lampe = true;
-                compteurl = 0;
-                compteurl++;
-                compteury++;
-            }
-            else if (!yoga)
-            {
-                yoga = true;
-                compteury = 0;
-                compteurl++;
-                compteury++;
-            }
-            else if (compteurl < 2 && compteury < 2)
-            {
-                compteurl++;
-                compteury++;
-            }
-            else
-                GameOverFaitTrop();
-        } 
-        
-        else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D))
-        {
-            lampe = false;
-            if (!yoga && !fauteuil)
-            {
-                fauteuil = true;
-                yoga = true;
-                compteurf = 0;
-                compteurf++;
-                compteury = 0;
-                compteury++;
-            }
-            else if (!fauteuil)
-            {
-                fauteuil = true;
-                compteurf = 0;
-                compteurf++;
-                compteury++;
-            }
-            else if (!yoga)
-            {
-                yoga = true;
-                compteury = 0;
-                compteurf++;
-                compteury++;
-            }
-            else if (compteurf < 2 && compteury < 2)
-            {
-                compteurf++;
-                compteury++;
-            }
-            else
-                GameOverFaitTrop();
-        }
+        ButtonManager.Instance.CheckButtons();
 
-        else if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.Q))
-        {
-            yoga = false;
-            if (!lampe && !fauteuil)
-            {
-                fauteuil = true;
-                lampe = true;
-                compteurf = 0;
-                compteurf++;
-                compteurl = 0;
-                compteurl++;
-            }
-            else if (!fauteuil)
-            {
-                fauteuil = true;
-                compteurf = 0;
-                compteurf++;
-                compteurl++;
-            }
-            else if (!lampe)
-            {
-                lampe = true;
-                compteurl = 0;
-                compteurf++;
-                compteurl++;
-            }
-            else if (compteurf < 2 && compteurl < 2)
-            {
-                compteurf++;
-                compteurl++;
-            }
-            else
-                GameOverFaitTrop();
-        }
-
-        else
-        {
+        if (ButtonManager.Instance.NoButton)
             GameOverFaitRien();
-        }
+
+        if (ButtonManager.Instance.IsTooMany)
+            GameOverFaitTrop();
     }
 
     public void GameOverFaitRien()
