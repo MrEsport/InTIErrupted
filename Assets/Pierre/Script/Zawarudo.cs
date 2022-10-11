6ee989fefb625f3a336f858301cf6573fce0d4a5
illianes <pierremariotte98@mail.com>
2022-10-11 11:26:58 +0200

    TextMeshPro

--- Assets/Pierre/Script/Zawarudo.cs
+++ Assets/Pierre/Script/Zawarudo.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Zawarudo : MonoBehaviour
 {
-    public float timer = 0.0f;
+    public int timer = 0;
+    public TextMeshProUGUI chronoCompteur;
+
 
     void Update()
     {
         timer += Time.deltaTime;
-        if (timer >= 5)
-        {
-            CheckButtons();
-            timer = 0;
-        }
+        chronoCompteur.text = timer.ToString();
     }
 }
