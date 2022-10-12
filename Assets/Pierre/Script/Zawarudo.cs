7bb1513f808a22286c4d293fc1c6f4789bf7946d
illianes <pierremariotte98@mail.com>
2022-10-12 09:49:27 +0200

    update chrono

--- Assets/Pierre/Script/Zawarudo.cs
+++ Assets/Pierre/Script/Zawarudo.cs
@@ -6,12 +6,31 @@ using TMPro;
 public class Zawarudo : MonoBehaviour
 {
     public float timer = 0;
-    public TextMeshProUGUI chronoCompteur;
+    private float timerEvent = 0;
+    private bool isCalculated = false;
 
+    public TextMeshProUGUI chronoCompteur;
+    public GameObject charaCall;
+
+    void Start()
+    {
+        timerEvent = Random.Range(8, 12);
+
+    }
 
     void Update()
     {
         timer += Time.deltaTime;
         chronoCompteur.text = timer.ToString();
+
+        if (timer >= 5 && !isCalculated)
+            isCalculated = true;
+
+        if (timer >= timerEvent)
+        {
+            timer = 0;
+            timerEvent = Random.Range(8, 12);
+            isCalculated = false;
+        }
     }
 }
