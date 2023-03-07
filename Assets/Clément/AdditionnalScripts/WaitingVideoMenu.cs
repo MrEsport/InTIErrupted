cd9325f607bbbed7901c00e2d818c2f74e8af61b
MrEsport <lucasespositonc@gmail.com>
2023-03-07 19:21:14 +0100

    Fix: Main Menu video skip

--- Assets/Clément/AdditionnalScripts/WaitingVideoMenu.cs
+++ Assets/Clément/AdditionnalScripts/WaitingVideoMenu.cs
@@ -1,109 +1,102 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 using UnityEditor;
 
 public class WaitingVideoMenu : MonoBehaviour
 {
-
     public GameObject playingVideo;
     public GameObject canvas;
     public GameObject SoundManager;
     public string[] guids2;
     public AudioClip[] moansAudio;
 
-    public List<UnityEngine.Video.VideoClip> video;
+    public List<VideoClip> video;
 
+    public float waitTimeAmount = 120f;
     public bool hasWait = false;
+    private bool isVideoPlaying = false;  // false = main menu | true = video mode
 
-    private bool MainMenuMode = false;  // false = main menu | true = video mode
+    private Coroutine waitRoutine = null;
+
+    private VideoPlayer videoPlayer; 
 
     // Start is called before the first frame update
     void Start()
     {
-
+        videoPlayer = playingVideo.gameObject.GetComponent<VideoPlayer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        if (!hasWait && !MainMenuMode) {
-            StartCoroutine(Waiting());
-           // Debug.Log("Si ce message est envoyé plusieurs fois, alors ça bug.");
+        if (!hasWait && !isVideoPlaying && waitRoutine == null)
+        {
+            waitRoutine = StartCoroutine(Waiting());
         }
 
         if (Input.GetKeyUp(KeyCode.Space))
         {
-            if (MainMenuMode)
+            if (isVideoPlaying)
             {
-                playingVideo.gameObject.GetComponent<UnityEngine.Video.VideoPlayer>().Stop();
+                videoPlayer.Stop();
                 canvas.SetActive(true);
                 SoundManager.SetActive(true);
                 hasWait = false;
-                MainMenuMode = !MainMenuMode;
+                isVideoPlaying = !isVideoPlaying;
             }
-            else if (!MainMenuMode)
+            else
             {
                 int nbAlea;
                 nbAlea = Random.Range(0, video.Count);
 
-                Debug.Log(nbAlea);
-                Debug.Log(playingVideo.gameObject.GetComponent<UnityEngine.Video.VideoPlayer>().length);
-                
+                //Debug.Log(nbAlea);
+                //Debug.Log(videoPlayer.length);
 
-                playingVideo.gameObject.GetComponent<UnityEngine.Video.VideoPlayer>().clip = video[nbAlea];
-                    playingVideo.gameObject.GetComponent<UnityEngine.Video.VideoPlayer>().Play();
-                    canvas.SetActive(false);
+                videoPlayer.clip = video[nbAlea];
+                videoPlayer.Play();
+                canvas.SetActive(false);
                 SoundManager.SetActive(false);
                 hasWait = true;
-                MainMenuMode = !MainMenuMode;
-                
+                isVideoPlaying = !isVideoPlaying;
             }
-
-            
         }
 
-        if (hasWait && !MainMenuMode)
+        if (hasWait && !isVideoPlaying)
         {
             int nbAlea;
             nbAlea = Random.Range(0, video.Count);
 
-            Debug.Log(nbAlea);
-            Debug.Log(playingVideo.gameObject.GetComponent<UnityEngine.Video.VideoPlayer>().length);
+            //Debug.Log(nbAlea);
+            //Debug.Log(videoPlayer.length);
 
-
-            playingVideo.gameObject.GetComponent<UnityEngine.Video.VideoPlayer>().clip = video[nbAlea];
-            playingVideo.gameObject.GetComponent<UnityEngine.Video.VideoPlayer>().Play();
+            videoPlayer.clip = video[nbAlea];
+            videoPlayer.Play();
             canvas.SetActive(false);
             SoundManager.SetActive(false);
 
-            MainMenuMode = !MainMenuMode;
+            isVideoPlaying = !isVideoPlaying;
         }
 
-        if (playingVideo.gameObject.GetComponent<UnityEngine.Video.VideoPlayer>().time >= playingVideo.gameObject.GetComponent<UnityEngine.Video.VideoPlayer>().length-0.1)
+        if (videoPlayer.time >= videoPlayer.length - 0.1)
         {
-            // playingVideo.gameObject.GetComponent<UnityEngine.Video.VideoPlayer>().Pause();
+            // videoPlayer.Pause();
 
             int nbAlea;
             nbAlea = Random.Range(0, video.Count);
 
-            Debug.Log(nbAlea);
+            //Debug.Log(nbAlea);
 
-            playingVideo.gameObject.GetComponent<UnityEngine.Video.VideoPlayer>().clip = video[nbAlea];
-            playingVideo.gameObject.GetComponent<UnityEngine.Video.VideoPlayer>().Play();
+            videoPlayer.clip = video[nbAlea];
+            videoPlayer.Play();
         }
 
         IEnumerator Waiting()
         {
-            yield return new WaitForSeconds(120f);
-            hasWait=true;
+            yield return new WaitForSeconds(waitTimeAmount);
+            hasWait = true;
+            waitRoutine = null;
         }
-
-
-
-
     }
-
-
 }
